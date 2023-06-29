f117173b6d2379982529db8927b4bc48ada642da
Ardumine <61353807+Ardumine@users.noreply.github.com>
2023-06-29 09:32:00 +0100

    Add files via upload

---
+++ a.bat
@@ -0,0 +1,9 @@
+@echo OFF
+winget install Git.Git
+
+git clone https://github.com/Ardumine/PAP_vitor_servidor
+git clone https://github.com/Ardumine/PAP_vitor_WinForms
+git clone https://github.com/Ardumine/PAP_vitor_camera
+git clone https://github.com/Ardumine/PAP_vitor_Robo
+git clone https://github.com/Ardumine/PAP_vitor_microbit
+echo OK
